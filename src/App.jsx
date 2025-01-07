b2ba3f14096b68b4a089ac2443e1322ddf5a81ae
Francesca-palmeri <francesca.palmeri.c@gmail.com>
2025-01-07 15:41:39 +0100

    esercizio 1 e 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ButtonComponent from './components/ButtonComponent'
+import ImageComponent from './components/ImageComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
+    <ButtonComponent ChangeBtn= 'Salva'/>
+    <ButtonComponent ChangeBtn= 'Importa'/>
+    <ButtonComponent ChangeBtn= 'Rimuovi'/>
+    
+    <div>
+      <ImageComponent src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Facebook_icon.svg" alt="Facebook logo"/>
+      <ImageComponent src="https://www.shareicon.net/data/256x256/2017/06/22/887584_logo_512x512.png" alt="Twitter logo"/>
+      <ImageComponent src="https://cdn.iconscout.com/icon/free/png-256/free-youtube-logo-icon-download-in-svg-png-gif-file-formats--social-media-70-flat-icons-color-pack-logos-432560.png?f=webp&w=256" alt="Youtube logo"/>
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,11 @@
+const ButtonComponent = function(props){
+    return(
+        <>
+        <button>
+            <span>{props.ChangeBtn}</span>
+        </button>
+        </>
+    )
+}
+
+export default ButtonComponent
